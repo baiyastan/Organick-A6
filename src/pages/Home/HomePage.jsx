0d803effab750f653d8cf5785f668d85784ab324
Jakyp-05 <jakypjumagulov080gmail.com>
2023-12-02 12:06:54 +0600

    added new Section folder

---
+++ src/pages/Home/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Hero from "../../Components/Hero/Hero";
+import Section2 from "../../Components/Section2/Section2";
+import AboutUs from "../../Components/AboutUs/AboutUs";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Hero />
+      <Section2 />
+      <AboutUs />
+    </div>
+  );
+};
+
+export default HomePage;
