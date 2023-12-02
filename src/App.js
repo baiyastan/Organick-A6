a093d63ff81890907653be3f0ae14e2cba1092bb
Bayastan <bsbaha195@gmail.com>
2023-12-02 12:05:11 +0600

    added image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import AbountImage from "./assets/image/Photo.png";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <img src={AbountImage} alt="image" />
+    </div>
+  );
 }
 
 export default App;
